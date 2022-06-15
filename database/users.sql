ed68a5b33966e6bbdf0a9b2101ee1dcac5298b57
FathyhQP <107481365+FathyhQP@users.noreply.github.com>
2022-06-15 21:52:29 +0700

    Add files via upload

---
+++ database/users.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 15, 2022 at 04:50 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `webbackend`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(1, 'fathyh qp', 'fathyh@mail.com', '$2y$10$lLA/qj27Jqu5G1Xu0QyRq.ev58XMytkSGdbHFZaxnFdzIiGIg7Wr2', '2022-06-14 02:41:25', '2022-06-14 02:41:25'),
+(2, 'Qorie1771', 'Qorie15@gmail.com', '$2y$10$2lUw59KcEWyqb532U.Px6./4YP4CPPG26txC6J84kGSivIb9s/BNy', '2022-06-15 07:48:30', '2022-06-15 07:48:30'),
+(3, 'coba123', 'coba@gmail.com', '$2y$10$cSHtgwm/HY.DCiu9jhzE9.Unhh8kae/sdS5Ysl7frpHJmQXu2SvR6', '2022-06-15 07:49:20', '2022-06-15 07:49:20'),
+(4, 'nova123', 'nova@gmail.com', '$2y$10$YcaIZ91fTXrJDuTEyXitR.dLLInYjOECyPPkugws1nbXHDmMukt2W', '2022-06-15 07:49:49', '2022-06-15 07:49:49'),
+(5, 'dwi234', 'dwi1@gmail.com', '$2y$10$PUCQVvmg/aa0CpO.Kv76/ONlRhUMd.swN434rPTnbpIwhc5epIqwq', '2022-06-15 07:50:04', '2022-06-15 07:50:04');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
